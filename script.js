030bf26a3a1b8cd8212887043acb4b5d186c0ad1
gbilog <gerald.ilog@gmail.com>
2017-02-09 19:39:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,206 @@
+//STEP 1: convert to Camel Case notation
+/*
+var someMonth;
+function theMonth(currentMonth);
+var summerMonth;
+function myLibrary;
+*/
+
+//STEP 2: examples of numeric literal, string literal, and boolean literal
+/*
+27               // a numeric literal
+"Who are you?"   // a string literal
+true             // a boolean literal
+null             // literal null 
+*/
+
+//STEP 3
+/*
+var complexExpression1 = "example sentence starts " + varStoredExpression;
+var complexMath = (x + y + z)/ 5;
+*/
+
+//STEP 4
+//var strFirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var strZipcode;
+//var intYourAge;
+//var strReferralSource;
+//var boolMayWeContact;
+
+//STEP 5: declare, assign, multiple var assignments
+/*
+var strFirstName;
+strFirstName = "Gerald";
+
+var strLastName = "Ilog";
+
+var strAddress;
+strAddress = "9474 Questa Pointe";
+
+var strCity = "Vallejo", strState = "CA", strZipcode = "92126", intYourAge = 36, strReferralSource = "website", boolMayWeContact = true;
+*/
+
+
+//STEP 6: coercion
+/*
+var number1 = 27;
+var string1 = "My favorite number is ";
+var verify = true;
+
+window.console.log(string1 + number1);
+window.console.log(string1 + verify);
+window.console.log(number1 + verify);
+*/
+
+//STEP 7: hoisting
+/*
+var string1 = "My favorite number is ";
+window.console.log(string1 + number1);
+var number1 = 27;
+*/
+
+//STEP 8: is string valid?
+//no, but fixed with escape sequences
+/*
+var someString = 'Who once said. \"Only two things are intinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log(someString);
+*/
+
+//STEP 9: null and undefined
+/*
+var x = null;
+var y;
+window.console.log(x);
+window.console.log(y);
+*/
+
+//STEP 10: typeof operator
+/*
+var name = "Gerald Ilog";
+var age = 900;
+var verify = false;
+var workstation = new Object;
+var y;
+
+window.console.log(typeof name);
+window.console.log(typeof age);
+window.console.log(typeof verify);
+window.console.log(typeof workstation);
+window.console.log(typeof y);
+*/
+
+//STEP 11: alert with concatenator
+/*
+var name = "Zak Ruvalcaba";
+window.alert("Hello "+ name + ", welcome to the JavaScript class!");
+*/
+
+
+//STEP 12:
+/*
+var name = "Gerald Ilog";
+window.alert("Hello "+ name + ", welcome to the JavaScript class!");
+*/
+
+//STEP 13
+/*
+var name = "Gerald Ilog";
+var course = "JavaScript";
+window.alert("Hello "+ name + ", welcome to the "+ course+ " class!");
+*/
+
+//STEP 14
+/*
+var name = "Gerald Ilog";
+var course = "JavaScript";
+window.alert("Hello "+ name + ". \nWelcome to the "+ course+ " class!");
+*/
+
+//STEP 15
+//var name = window.prompt("What is your name?");
+
+//STEP 16
+//var course = window.prompt("What course are you taking?");
+
+//STEP 17
+/*
+var x = 10;
+var y = 20;
+
+window.console.log(x+y);
+*/
+
+
+//STEP 18
+/*
+var x = 20;
+x += 20;
+window.console.log(x);
+*/
+
+//STEP 19
+/*
+var x = 20;
+x *= 5;
+
+window.console.log(x);
+*/
+
+//STEP 20;
+/*
+var x = 20%3;
+x /= 1;
+window.console.log(x);
+*/
+
+//STEP 21
+/*
+var x = 5;
+var y = 25/5;
+window.console.log(x==y);
+*/
+
+//STEP 22
+/*
+var myName = "Gerald";
+window.console.log(myName != "Gerald");
+*/
+
+//STEP 23
+/*
+var widget = new Object;
+window.console.log(typeof widget);
+
+
+//STEP 24
+window.alert(widget instanceof Object);
+*/
+
+//STEP 25
+/*
+var widget = new Object;
+window.console.log(typeof widget);
+window.alert(widget instanceof Number);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
